73cd989e38459128e83ae51610c3dd69b505fda6
Anatolii Stoliarenko <nokiasto@gmail.com>
2024-07-25 11:18:41 +0200

    fixed contact form

--- src/app/contact/contact.component.ts
+++ src/app/contact/contact.component.ts
@@ -1,6 +1,7 @@
-import { HttpClient } from '@angular/common/http';
 import { Component } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+
+import { FormValues } from './contact.model';
 
 @Component({
   selector: 'app-contact',
@@ -9,10 +10,11 @@ import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 })
 export class ContactComponent {
   contactForm: FormGroup;
+  emailForContact = 'anatolii.stoliarenko@gmail.com';
   submitSuccess = false;
   submitError = false;
 
-  constructor(private fb: FormBuilder, private http: HttpClient) {
+  constructor(private fb: FormBuilder) {
     this.contactForm = this.fb.group({
       name: ['', Validators.required],
       email: ['', [Validators.required, Validators.email]],
@@ -23,18 +25,61 @@ export class ContactComponent {
 
   onSubmit() {
     if (this.contactForm.valid) {
-      const formData = this.contactForm.value;
-      this.http.post('/', formData).subscribe({
-        next: () => {
-          this.submitSuccess = true;
-          this.submitError = false;
-          this.contactForm.reset();
-        },
-        error: () => {
-          this.submitSuccess = false;
-          this.submitError = true;
-        },
-      });
+      const mailtoLink = this.emailtoLink(this.contactForm.value);
+      window.open(mailtoLink, '_blank');
+      this.submitSuccess = true;
+
+      //sending message to netlify form-service
+      // const formData = this.contactForm.value;
+      // this.http.post('/', formData).subscribe({
+      //   next: () => {
+      //     this.submitSuccess = true;
+      //     this.submitError = false;
+      //     this.contactForm.reset();
+      //   },
+      // error: () => {
+      //   this.submitSuccess = false;
+      //   this.submitError = true;
+      // },
+      // });
     }
+  }
+
+  emailtoLink(formValues: FormValues): string {
+    const subject = this.emailSubject(formValues.name);
+    const body = this.emailBody(formValues);
+    const mailtoLink = `mailto:${
+      this.emailForContact
+    }?subject=${encodeURIComponent(subject)}&body=${encodeURIComponent(body)}`;
+    return mailtoLink;
+  }
+
+  emailSubject(name: string): string {
+    const subject = `Contact from ${name}`;
+    return subject;
+  }
+
+  emailBody(formValues: FormValues): string {
+    const body = `
+  Hello Anatolii,
+  
+  I hope this message finds you well.
+  
+  My name is ${formValues.name}, and I recently visited your website. I am interested in discussing potential collaboration opportunities for web development projects.
+  
+  Please find my contact details below:
+  - Name: ${formValues.name}
+  - Email: ${formValues.email}
+  - Phone: ${formValues.phone}
+  
+  Message:
+  ${formValues.message}
+  
+  I look forward to hearing from you and discussing this further.
+  
+  Best regards,
+  ${formValues.name}
+  `;
+    return body;
   }
 }
